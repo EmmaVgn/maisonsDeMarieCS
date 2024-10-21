d9a2fdde4561a963d9dc9f665d3cc84b47ab65b7
EmmaVgn <emma.vigneron@outlook.fr>
2024-10-21 11:17:59 +0200

    Mise en place de l'enregistrement d'un user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,53 +4,70 @@ use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
+
     #[ORM\Column(length: 180)]
+
     private ?string $email = null;
+    
     /**
      * @var list<string> The user roles
      */
     #[ORM\Column]
     private array $roles = [];
+
     /**
      * @var string The hashed password
      */
     #[ORM\Column]
     private ?string $password = null;
+
     #[ORM\Column(length: 255)]
     private ?string $firstname = null;
+
     #[ORM\Column(length: 255)]
     private ?string $lastname = null;
+
     #[ORM\Column(length: 255)]
-    private ?string $adress = null;
+    private ?string $address = null;
+
     #[ORM\Column(length: 255)]
     private ?string $postalCode = null;
+
     #[ORM\Column(length: 255)]
     private ?string $city = null;
     #[ORM\Column]
     private ?bool $isVerified = false;
     #[ORM\Column(length: 255)]
     private ?string $phone = null;
+
     public function getId(): ?int
     {
         return $this->id;
     }
+
     public function getEmail(): ?string
     {
         return $this->email;
     }
+
     public function setEmail(string $email): static
     {
         $this->email = $email;
+
         return $this;
     }
+
     /**
      * A visual identifier that represents this user.
      *
@@ -60,6 +77,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         return (string) $this->email;
     }
+
     /**
      * @see UserInterface
      *
@@ -68,10 +86,12 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function getRoles(): array
     {
         $roles = $this->roles;
+        
         // guarantee every user at least has ROLE_USER
         $roles[] = 'ROLE_USER';
         return array_unique($roles);
     }
+
     /**
      * @param list<string> $roles
      */
@@ -80,6 +100,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         $this->roles = $roles;
         return $this;
     }
+
     /**
      * @see PasswordAuthenticatedUserInterface
      */
@@ -87,6 +108,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         return $this->password;
     }
+
     public function setPassword(string $password): static
     {
         $this->password = $password;
@@ -100,67 +122,88 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
     }
+
     public function getFirstname(): ?string
     {
         return $this->firstname;
     }
+
     public function setFirstname(string $firstname): static
     {
         $this->firstname = $firstname;
+
         return $this;
     }
+
     public function getLastname(): ?string
     {
         return $this->lastname;
     }
+
     public function setLastname(string $lastname): static
     {
         $this->lastname = $lastname;
+
         return $this;
     }
-    public function getAdress(): ?string
+
+    public function getAddress(): ?string
     {
-        return $this->adress;
+        return $this->address;
     }
-    public function setAdress(string $adress): static
+
+    public function setAddress(string $address): static
     {
-        $this->adress = $adress;
+        $this->address = $address;
+
         return $this;
     }
+
     public function getPostalCode(): ?string
     {
         return $this->postalCode;
     }
+
     public function setPostalCode(string $postalCode): static
     {
         $this->postalCode = $postalCode;
+
         return $this;
     }
+
     public function getCity(): ?string
     {
         return $this->city;
     }
+
     public function setCity(string $city): static
     {
         $this->city = $city;
+
         return $this;
     }
+
     public function isVerified(): ?bool
     {
         return $this->isVerified;
     }
+
     public function setVerified(bool $isVerified): static
     {
         $this->isVerified = $isVerified;
+
         return $this;
     }
+
     public function getPhone(): ?string
     {
         return $this->phone;
     }
+
     public function setPhone(string $phone): static
     {
         $this->phone = $phone;
+
         return $this;
     }
 }
