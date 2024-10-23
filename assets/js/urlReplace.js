72d3d81219f776bcf17c78e082974aaeef284efd
EmmaVgn <emma.vigneron@outlook.fr>
2024-10-23 09:54:19 +0200

    add fichiers versionnée

--- assets/js/urlReplace.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('turbo:load', () => {
-    const urlParams = new URLSearchParams(window.location.search);
-    if (urlParams.has('adId') || urlParams.has('userId')) {
-        // Remplacer l'URL avec les paramètres nettoyés
-        const newUrl = window.location.pathname;
-        window.history.replaceState({}, '', newUrl);
-    }
-});
